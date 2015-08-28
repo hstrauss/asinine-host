c925c5ee314dbdbb13d288116a70a52897e93b67
Heinrich Strauss <git@hstrauss.co.za>
2015-08-28 14:35:14 +0200

    (2015.08.28-14h35) updates: {}

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ var pg = require('pg');
 var dns = require('dns');
 var bunyan = require('bunyan');
 var PORT = 2000;
+var router = require('./routes/router');
+var handler = require('./routes/handler');
 //
 var log = bunyan.createLogger({
-    name: 'myserver',
+    name: 'hstools.api',
     serializers: {
         req: bunyan.stdSerializers.req,
         res: bunyan.stdSerializers.res
@@ -16,10 +18,19 @@ var log = bunyan.createLogger({
 //
 var server = http.createServer(handleRequest);
 server.listen(PORT, server_log);
+router.register('/', function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.write('Hello World');
+    res.close();
+});
 //
-function handleRequest(request, response) {
-    response.end('It Works!! Path Hit: ' + request.url);
-    debug('Connected from %s', request.url);
+function handleRequest(req, res) {
+    // res.end('It Works!! Path Hit: ' + req.url);
+    // debug('Connected from %s', req.url);
+    handler = router.route(req);
+    handler.process(req, res);
 }
 
 function server_log() {
