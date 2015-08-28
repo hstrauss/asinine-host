94b08f1f3c24689525f1d9e6d13d35b9111e015c
Heinrich Strauss <git@hstrauss.co.za>
2015-08-28 14:12:19 +0200

    (2015.08.28-14h12) updates: {}

--- api/index.js
+++ api/index.js
@@ -2,10 +2,20 @@
 var http = require('http');
 var pg = require('pg');
 var dns = require('dns');
+var bunyan = require('bunyan');
 var PORT = 2000;
+//
+var log = bunyan.createLogger({
+    name: 'myserver',
+    serializers: {
+        req: bunyan.stdSerializers.req,
+        res: bunyan.stdSerializers.res
+    }
+});
+//
 var server = http.createServer(handleRequest);
 server.listen(PORT, server_log);
-
+//
 function handleRequest(request, response) {
     response.end('It Works!! Path Hit: ' + request.url);
 }
